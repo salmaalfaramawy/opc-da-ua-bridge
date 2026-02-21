b95d626547b41bc534d3eb11c5b4e8f5233bc9d6
salmaalfaramawy <salmakh.1627@gmail.com>
2026-02-22 01:32:41 +0300

    uploaded code

---
+++ bridge/config.py
@@ -0,0 +1,10 @@
+CONFIG = {
+    # OPC DA
+    "da_server": "Kepware.KEPServerEX.V6",
+    "group_name": "Channel2.Device1",
+
+    # OPC UA
+    "endpoint": "opc.tcp://0.0.0.0:4840/freeopcua/server/",
+    "namespace_uri": "https://example.com/opcua",
+    "ua_object_name": "Micrologix 1400 Series B",
+}
